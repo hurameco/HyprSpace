f08052c9d0a08bf91d22cc1f8fd9383d66ffc544
Hugo Coelho <hurameco@gmail.com>
2025-08-19 23:56:06 +0100

    [Fixed] 20-git echo commands missing -e flag

--- scripts/20-git.sh
+++ scripts/20-git.sh
@@ -3,9 +3,9 @@
 logo
 rnotice "Configuring git [1/1]\n\n"
 
-echo "Enter your git username:\n> "
+echo -e "Enter your git username:\n> "
 read -r git_username < /dev/tty
-echo "Enter your git email:\n> "
+echo -e "Enter your git email:\n> "
 read -r git_email < /dev/tty
 
 git config --global user.name "$git_username"
