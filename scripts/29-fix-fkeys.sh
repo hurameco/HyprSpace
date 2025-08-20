a9ff38035dd9b6c2abc6e041d3ff1954312597b1
Hugo Coelho <hurameco@gmail.com>
2025-08-20 01:18:37 +0100

    [Removed] TZUpdate from sudoers.d

--- scripts/29-fix-fkeys.sh
+++ scripts/29-fix-fkeys.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-if [[ ! -f /etc/modprobe.d/hid_apple.conf ]]; then
+if [[ -f /etc/modprobe.d/hid_apple.conf ]]; then
     logo
     rnotice "F Keys :: [1/1] :: Fixing F Keys for Apple Keyboards"
-    echo "options hid_apple fnmode=2" | sudo tee /etc/modprobe.d/hid_apple.conf
+    echo "options hid_apple fnmode=1" | sudo tee /etc/modprobe.d/hid_apple.conf
     success "F Keys successfully fixed for Apple Keyboards!"
 
-  # Rely on install/login.sh to do the rebuild
-  # sudo mkinitcpio -P
+    # Rely on install/login.sh to do the rebuild
+    # sudo mkinitcpio -P
 fi
